64a69676c2f33e65a4b27025ebeb31fa6c478285
ChlorineKorin <kharlasuncion1010@gmail.com>
2025-04-17 23:36:09 +0800

    Initial Commit

---
+++ Final Activity #1/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button2 = New Button()
+        Label1 = New Label()
+        Button3 = New Button()
+        Panel1 = New Panel()
+        Button1 = New Button()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Button2
+        ' 
+        Button2.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button2.BackColor = Color.Black
+        Button2.BackgroundImage = My.Resources.Resources.Polygon_1
+        Button2.BackgroundImageLayout = ImageLayout.Zoom
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.ForeColor = Color.Black
+        Button2.Location = New Point(89, 139)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(62, 47)
+        Button2.TabIndex = 2
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.FlatStyle = FlatStyle.Flat
+        Label1.Font = New Font("Digital-7 Mono", 60F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.Lime
+        Label1.Location = New Point(2, 44)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(330, 80)
+        Label1.TabIndex = 9
+        Label1.Text = "00:00:00"
+        ' 
+        ' Button3
+        ' 
+        Button3.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button3.BackColor = Color.Black
+        Button3.BackgroundImage = My.Resources.Resources.Restart
+        Button3.BackgroundImageLayout = ImageLayout.Zoom
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.ForeColor = Color.Black
+        Button3.Location = New Point(194, 131)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(60, 63)
+        Button3.TabIndex = 11
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Button1)
+        Panel1.Location = New Point(-16, -8)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(370, 49)
+        Panel1.TabIndex = 12
+        ' 
+        ' Button1
+        ' 
+        Button1.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button1.BackColor = Color.Black
+        Button1.BackgroundImage = My.Resources.Resources.Vector
+        Button1.BackgroundImageLayout = ImageLayout.Zoom
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.ForeColor = Color.Black
+        Button1.Location = New Point(330, 16)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(24, 24)
+        Button1.TabIndex = 13
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaptionText
+        ClientSize = New Size(347, 222)
+        Controls.Add(Panel1)
+        Controls.Add(Button3)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Form1"
+        ShowIcon = False
+        Text = "Timer"
+        Panel1.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Final Activity #1/Form1.vb
@@ -0,0 +1,114 @@
+﻿Imports System.Threading.Tasks
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    <DllImport("Gdi32.dll", EntryPoint:="CreateRoundRectRgn")>
+    Private Shared Function CreateRoundRectRgn(
+        ByVal nLeftRect As Integer, ByVal nTopRect As Integer,
+        ByVal nRightRect As Integer, ByVal nBottomRect As Integer,
+        ByVal nWidthEllipse As Integer, ByVal nHeightEllipse As Integer
+    ) As IntPtr
+    End Function
+
+    Dim timerValue As Int32
+
+    Dim secTime As Int32 = 0
+    Dim minTime As Int32 = 0
+    Dim hrTime As Int32 = 0
+    Dim toggleStart As Boolean = False
+    Dim paused As Boolean = False
+
+    Private isDragging As Boolean = False
+    Private startPoint As Point
+
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        isDragging = True
+        startPoint = e.Location
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If isDragging Then
+            Dim newPoint As New Point(Me.Location.X + (e.X - startPoint.X), Me.Location.Y + (e.Y - startPoint.Y))
+            Me.Location = newPoint
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        isDragging = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button2.BackgroundImage = My.Resources.Polygon_1
+        Me.TopMost = True
+        Me.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Me.Width, Me.Height, 30, 30))
+    End Sub
+
+    Private Async Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If toggleStart = True Then
+            toggleStart = False
+            Button2.BackgroundImage = My.Resources.Polygon_1
+        Else
+            toggleStart = True
+            Button2.BackgroundImage = My.Resources.Group_1
+            If paused = True Then
+                Label1.ForeColor = Color.Lime
+                Await Task.Delay(1000)
+            End If
+        End If
+
+        While toggleStart
+            paused = True
+
+            If secTime >= 59 Then
+                minTime += 1
+                secTime = -1
+                If minTime < 10 Then
+                    minTime = "0" & minTime
+                End If
+            End If
+
+            If minTime >= 59 Then
+                hrTime += 1
+                minTime = 0
+                If hrTime < 10 Then
+                    hrTime = "0" & hrTime
+                End If
+            End If
+
+            If hrTime >= 24 Then
+                toggleStart = False
+            End If
+
+            secTime += 1
+
+            If secTime < 10 Then
+                secTime = "0" & secTime
+            End If
+
+            Label1.Text = hrTime.ToString("00") & ":" & minTime.ToString("00") & ":" & secTime.ToString("00")
+
+            Await Task.Delay(1000)
+        End While
+        If paused = True Then
+            Label1.ForeColor = Color.Red
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        toggleStart = False
+        paused = False
+        secTime = 0
+        minTime = 0
+        hrTime = 0
+        Label1.Text = "00:00:00"
+        Button2.BackgroundImage = My.Resources.Polygon_1
+        Label1.ForeColor = Color.Lime
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+
+    End Sub
+End Class
